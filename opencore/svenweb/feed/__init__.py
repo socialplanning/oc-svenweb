431ebc873e08cc834f2afc8483d5a5763ae5b9cb
Ethan Jucovy <ejucovy@gmail.com>
2011-12-09 10:15:43 -0500

    bash my head against six bricks walls for two hours to finally get the damn framework responding with a basic wireframe

---
+++ opencore/svenweb/feed/__init__.py
@@ -0,0 +1,116 @@
+import feedparser
+import urllib2
+
+from opencore.feed.base import BaseFeedAdapter
+from opencore.feed.base import FeedItemResponses
+from opencore.feed.interfaces import IFeedData
+from opencore.interfaces import IProject
+from opencore.nui.wiki.utils import unescape # XXX this should not live here!
+from zope.component import adapts
+from zope.interface import implements
+
+from opencore.utility.interfaces import IProvideSiteConfig
+from zope.component import getUtility
+
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from opencore.browser.blankslate import BlankSlateViewlet
+import opencore.feed.browser
+import os
+
+class SvenwebSummaryViewlet(BlankSlateViewlet):
+    blank_template = ZopeTwoPageTemplateFile('blank_slate.pt')
+    template = os.path.join(os.path.dirname(opencore.feed.browser.__file__),
+                            'feed_snippet.pt')
+    template = ZopeTwoPageTemplateFile(template)
+    adapter_name = 'svenweb'
+
+    sort_order = 101
+
+    def is_blank(self):
+        return not hasattr(self, 'feed') or not self.feed.items
+
+class SvenwebFeedAdapter(BaseFeedAdapter):
+    implements(IFeedData)
+    adapts(IProject)
+
+    title = "Wikis"
+    itemstitle = "recent edits"
+
+    @property
+    def link(self):
+        return "%s/wikis/" % self.context.absolute_url()
+
+    @property
+    def items(self):
+        if not hasattr(self, '_items'):
+            self.populate_items()
+        return self._items
+
+    def populate_items(self, n_items=5):
+        self._items = []
+        base_uri = getUtility(IProvideSiteConfig).get('svenweb uri')
+        uri = '%s/feed/' % base_uri
+
+        # pull down the feed with the proper cookie
+        req = urllib2.Request(uri)
+        req.add_header('X-Openplans-Project', self.context.getId())
+        cookie = self.context.REQUEST.get_header('Cookie')
+        if cookie:
+            req.add_header('Cookie', cookie)
+        try:
+            feed = urllib2.urlopen(req).read()
+        except urllib2.HTTPError:
+            # fail silently for now
+            feed = ''
+
+        # parse with feedparser
+        feed = feedparser.parse(feed)
+        # feedparser takes care of HTML sanitization:
+        # http://www.feedparser.org/docs/html-sanitization.html
+        try:
+            title = feed.feed.title
+        except AttributeError:
+            # this means the uri is not a feed (or something?)
+            return
+
+        # maybe this should be done after comments?
+        # feed.entries.sort(key=date_key) # they appeared sorted already?
+        feed.entries = feed.entries[:n_items]
+
+        for entry in feed.entries:
+            title = entry.title
+            if not title.strip():
+                title = unescape(entry.summary) # XXX unescaping seems weird
+                
+            # see #2845 - it seems the results returned by wordpress use
+            # the request URL to determine the items' link base
+            # and since we're using the "internal" link this will
+            # be wrong. so i'll just manually replace each item's
+            # 'base href' with the right one.
+            #
+            # we have to do this for the link to the entry itself and also
+            # the link to the entry's comments/replies, if it exists
+            if entry.link.startswith(base_uri):
+                entry.link = entry.link[len(base_uri):] # lstrip base_uri
+                entry.link = '/'.join((self.link.rstrip('/'), entry.link.lstrip('/')))
+
+            num_comments = int(entry.get('slash_comments', 0))
+
+            if num_comments > 0:
+                if entry.comments.startswith(base_uri): # see #2845
+                    entry.comments = entry.comments[len(base_uri):]
+                    entry.comments = '/'.join((self.link.rstrip('/'), 
+                                               entry.comments.lstrip('/')))
+                response = FeedItemResponses(num_comments,
+                                             entry.comments,
+                                             'comment')
+            else:
+                response = None
+
+            self.add_item(title=title,
+                          description=entry.summary,
+                          link=entry.link,
+                          author=entry.author,
+                          authorURL=self.memberURL(entry.userid),
+                          pubDate=entry.date,
+                          responses=response)
